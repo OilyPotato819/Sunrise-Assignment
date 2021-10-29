5ebe61bcf9d004baff2476621d2fae1a2def609b
OilyPotato819 <90284101+OilyPotato819@users.noreply.github.com>
2021-10-29 11:32:15 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Sunrise Assignment
+
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 400;
+cnv.height = 400;
+
+let cloud = document.getElementById("cloud-img");
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 400, 300);
+
+ctx.fillStyle = "red";
+ctx.beginPath();
+ctx.arc(200, 300, 30, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.fillStyle = "green";
+ctx.fillRect(0, 300, 400, 100);
+
+ctx.drawImage(cloud, 200, 300);
